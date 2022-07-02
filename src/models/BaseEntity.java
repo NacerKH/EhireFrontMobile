bbaf74fb3606fead6e59ce35e0b1301e39ad014c
khalifa <mednaceur199530@gmail.com>
2022-07-02 16:46:19 +0100

    fix name pack

--- src/models/BaseEntity.java
+++ src/models/BaseEntity.java
@@ -5,10 +5,70 @@
  */
 package models;
 
+import java.util.Date;
+
 /**
  *
  * @author LENOVO
  */
 public class BaseEntity {
     
+    // <editor-fold defaultstate="collapsed" desc="Attributes + Getters & Setters">
+
+    // <editor-fold defaultstate="collapsed" desc="Id">
+    int Id; 
+    public int getId() {
+        return Id;
+    }
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="CreatedDate">
+    Date CreatedDate; 
+     public Date getCreatedDate() {
+        return CreatedDate;
+    }
+
+    public void setCreatedDate(Date CreatedDate) {
+        this.CreatedDate = CreatedDate;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="UpdatedDate">
+     Date UpdatedDate; 
+     public Date getUpdatedDate() {
+        return UpdatedDate;
+    }
+
+    public void setUpdatedDate(Date UpdatedDate) {
+        this.UpdatedDate = UpdatedDate;
+    }
+    // </editor-fold>
+
+    
+    // </editor-fold>
+   
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    public BaseEntity() {
+        
+    }
+    public BaseEntity(int Id, Date CreatedDate, Date UpdatedDate) {
+        this.Id = Id;
+        this.CreatedDate = CreatedDate;
+        this.UpdatedDate = UpdatedDate;
+    }
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+    
+    // <editor-fold defaultstate="collapsed" desc="toString">
+    @Override
+    public String toString() {
+        return "BaseEntity{" + "Id=" + Id + ", CreatedDate=" + CreatedDate + ", UpdatedDate=" + UpdatedDate + '}';
+    }
+    // </editor-fold>
+    
+    // </editor-fold>
 }
--- src/services/CondidatEmlpoyeServices.java
+++ src/services/CondidatEmlpoyeServices.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Services;
+package services;
 
 import Utils.Statitics;
 import com.codename1.io.ConnectionRequest;
